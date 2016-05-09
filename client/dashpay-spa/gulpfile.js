c5c53314548ead600afa207b6385368a0bc30b2f
Andy Freer <andy@dash.org>
2016-05-09 10:50:57 +0100

    view event test on new spa client

--- client/dashpay-spa/gulpfile.js
+++ client/dashpay-spa/gulpfile.js
@@ -16,22 +16,25 @@ gulp.task('browserify', function() {
   .pipe(source(destFile))
   .pipe(gulp.dest(destFolder));
 });
-// Rerun the task when a file changes
-gulp.task('watch2', function() {
-  gulp.watch('./js/**/*', ['browserify']);
-  
-});
-gulp.task('watch', function() {
-  var bundler = watchify(sourceFile);
-  bundler.on('update', rebundle);
 
-  function rebundle() {
-    return bundler.bundle()
-      .pipe(source(destFile))
-      .pipe(gulp.dest(destFolder));
-  }
+//
+//// Rerun the task when a file changes
+//gulp.task('watch2', function() {
+//  gulp.watch('./js/**/*', ['browserify']);
+//
+//});
+//gulp.task('watch', function() {
+//  var bundler = watchify(sourceFile);
+//  bundler.on('update', rebundle);
+//
+//  function rebundle() {
+//    return bundler.bundle()
+//      .pipe(source(destFile))
+//      .pipe(gulp.dest(destFolder));
+//  }
+//
+//  return rebundle();
+//});
 
-  return rebundle();
-});
-
-gulp.task('default', ['browserify', 'watch2']);
+//gulp.task('default', ['browserify', 'watch2']);
+gulp.task('default', ['browserify']);
--- client/dashpay-spa/js/modules/auth/views/LoginView.js
+++ client/dashpay-spa/js/modules/auth/views/LoginView.js
@@ -3,5 +3,13 @@
 var marionette = require('backbone.marionette');
 
 module.exports = marionette.ItemView.extend({
-template:require('./LoginView.html')
+
+    template: require('./LoginView.html'),
+    events: { 'click #btn-login' : 'attemptLogin'},
+    onRender: function() {
+        alert('test');
+    },
+    attempLogin: function() {
+        alert('Login clicked');
+    }
 });
