a54c45e40817d6fdc65860f1034e547a8729f9da
Andy Freer <andy@dash.org>
2016-05-29 11:52:56 +0100

    Add Windows setup script

--- setup/build.sh
+++ setup/build.sh
@@ -1,6 +1,25 @@
-# BUILD ALL PROJECTS
+# CLEAN BUILD FOLDERS
+#------------------------------------------
 
-./clean.sh
+rm -rfv ../lib/dashpay-lib/dist
+rm -rfv ../lib/dashpay-wallet-sdk/dist
+rm -rfv ../lib/dashpay-wallet-client/dist
+rm -rfv ../lib/dashpay-checkout-sdk/dist
+rm -rfv ../client/dashpay-wallet-html/dist
+rm -rfv ../client/dashpay-spa/dist
+
+rm -rfv ../client/dashpay-spa/bundle.js
+
+rm -rfv ../lib/dashpay-lib/build
+rm -rfv ../lib/dashpay-wallet-sdk/build
+rm -rfv ../lib/dashpay-wallet-client/build
+rm -rfv ../lib/dashpay-checkout-sdk/build
+rm -rfv ../client/dashpay-wallet-html/build
+rm -rfv ../client/dashpay-spa/build
+
+
+# BUILD ALL PROJECTS
+#------------------------------------------
 
 # Build the SPA client
 (cd ../client/dashpay-spa && gulp)
